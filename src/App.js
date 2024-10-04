803c8dc68d034518984bf605f4077150def96a84
trinv1 <trinityvillaruel@gmail.com>
2024-10-04 08:47:21 +0000

    added todays time

--- src/App.js
+++ src/App.js
@@ -3,25 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-  <div>
-    <h1>Hello World!</h1>
+    <div className="App">        
+      
+     {/*displaying simple hello world message*/}
+    <h1>Hello World!</h1> 
+
+    {/*displaying time*/}
+    <h2>It is {new Date().toLocaleTimeString()}.</h2> 
+
   </div>
 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
   );
 }
 
