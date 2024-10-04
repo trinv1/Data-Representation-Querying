a15bbf618f7f3c701f78dc8d887cb4345cafdd22
trinv1 <trinityvillaruel@gmail.com>
2024-10-04 09:13:34 +0000

    Added nav bar and added routing logic so different components are diplayed when clicked

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header'; 
 import Content from './Components/Content';
 import Footer from './Components/Footer';
+import NavigationBar from './Components/NavBar';
 {/*Importing components*/}
 
 function App() {
   return (
-    <div className="App">        
-      
-      <Header /> {/*calling and displaying components contents*/}
-      <Content />
-      <Footer />
-
-  </div>
-
+      <Router>
+        <NavigationBar />
+        <Routes>
+          {/*Each route displays different component when clicked*/}
+          <Route path="/home" element={<Content />} />
+          <Route path="/read" element={<h1>Read Component</h1>} />
+          <Route path="/create" element={<h1>Create Component</h1>} />
+        </Routes>
+        <Footer />
+      </Router>
   );
 }
 
---
+++ src/Components/NavBar.js
@@ -0,0 +1,21 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+const NavigationBar = () => { {/*added navigation bar to page*/}
+  return (
+        <Navbar bg="primary" data-bs-theme="dark"> 
+          <Container>
+            <Navbar.Brand href="/">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/create">Create</Nav.Link>
+              <Nav.Link href="/read">Read</Nav.Link>
+            </Nav>
+          </Container>
+      </Navbar>
+  );
+};
+
+export default NavigationBar;
