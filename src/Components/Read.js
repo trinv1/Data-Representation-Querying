14db1e6dab32630f222507e4a35085a2328ac430
trinv1 <trinityvillaruel@gmail.com>
2024-10-18 08:40:11 +0000

    useEffect and useState added to Read component to get http request and store data returned

--- src/Components/Read.js
+++ src/Components/Read.js
@@ -1,36 +1,27 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Movies from "./Movies"; {/*importing movies*/}
 const Read = () => { {/*Read component*/}
 
+//use state used to store data returned from API and manage state of application
 
-{/*Constant variable to hold JSON data for movies*/}
-const data =
-[   
-    {
-      "Title": "Avengers: Infinity War", 
-      "Year": "2018",
-      "imdbID": "tt4154756",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-    },
-    {
-      "Title": "Captain America: Civil War",
-      "Year": "2016",
-      "imdbID": "tt3498820",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-    },
-    {
-      "Title": "World War Z",
-      "Year": "2013",
-      "imdbID": "tt0816711",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
-    }
-  ];
+  const[movies, setMovies] = useState([]);
+ 
+  useEffect(
+    ()=>{//using axios to talk on web for me
+      axios.get('https:jsonblob.com/api/jsonblob/1287718524221775872')//sending http request and run in background not causing app to freeze (asynchronous)
+      .then((response)=>{//got response back from http request
+        console.log(response.data);//logging to console
+        setMovies(response.data.movies)//pulling back in objects from json movies
+      })
+      .catch()
+    },[]//empty array to stop firing every time and only fire when things are updated
+  );
+
 
   return (
     <div>
-     <Movies myMovies={data}/> {/* Rendering the Movies component and passing 'data' as the 'myMovies' prop */}
+     <Movies myMovies={movies}/> {/* Rendering the Movies component and passing 'data' as the 'myMovies' prop */}
     </div>
   );
   };
