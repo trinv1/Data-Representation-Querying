20a690954419ccdd3b376cb86864da4aa7062192
trinv1 <trinityvillaruel@gmail.com>
2024-10-28 18:48:57 +0000

    added route that accepts name parameters

--- src/server.js
+++ src/server.js
@@ -10,7 +10,13 @@ app.listen(port, () => {//starting server and listening on specified port
     console.log(`Server is running on http://localhost:${port}`);//logging servers url to console
 });
 
-app.use((err, req, res, next) => {
+app.get('/hello/:name/:surname', (req, res) => {//defining a route for GET requests to '/hello/:name', where ':name' is a route parameter
+    const name = req.params.name;//extracting the parameters from the URL and stores it in a variable
+    const surname = req.params.surname;
+    res.send(`Hello ${name} ${surname}`);//sending a response with "Hello" followed by the provided name
+});
+
+app.use((err, req, res, next) => {//error handling
     console.error(err.stack);
     res.status(500).send('Something went wrong!');
 });
