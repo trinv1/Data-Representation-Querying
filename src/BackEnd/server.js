12d0b87be8627f81dc282132caaf2a06865e3a8f
trinv1 <trinityvillaruel@gmail.com>
2024-11-08 10:09:12 +0000

    updated json

--- src/BackEnd/server.js
+++ src/BackEnd/server.js
@@ -6,33 +6,33 @@ app.get('/', (req, res) =>{
    res.send('Hello World');
 });
 
-
 app.get('/api/movies', (req, res) => {//route for GET requests to the root URL ('/')
     const movies = [
-        {
-            "Title": "Avengers: Infinity War",
-            "Year": "2018",
-            "imdbID": "tt4154756",
-            "Type": "movie",
-            "Poster": "https://example.com/poster1.jpg"
-        },
-        {
-            "Title": "Captain America: Civil War",
-            "Year": "2016",
-            "imdbID": "tt3498820",
-            "Type": "movie",
-            "Poster": "https://example.com/poster2.jpg"
-        },
-        {
-            "Title": "World War Z",
-            "Year": "2013",
-            "imdbID": "tt0816711",
-            "Type": "movie",
-            "Poster": "https://example.com/poster3.jpg"
-        }
-    ];
+    {
+      "Title": "Avengers: Infinity War (server)",
+      "Year": "2018",
+      "imdbID": "tt4154756",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+    },
+    {
+      "Title": "Captain America: Civil War (server)",
+      "Year": "2016",
+      "imdbID": "tt3498820",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+    },
+    {
+      "Title": "World War Z (server)",
+      "Year": "2013",
+      "imdbID": "tt0816711",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
+    }
+];
     res.status(201).json({ myMovies: movies });//returning JSON with status 201 and rename array to 'myMovies'
 });
+
 
 app.listen(port, () =>{
     console.log(`Server is running on http://localhost:${port}`);
